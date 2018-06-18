457cff694de69c4b0c0f7a63c47c53c0267d5a24
ej <enrui.zhang@gmail.com>
2018-06-18 07:43:04 +0000

    Save every connect to global variable

--- srv/connect/app.js
+++ srv/connect/app.js
@@ -1,28 +1,33 @@
 #!/usr/bin/env node
 var WebSocketServer = require('websocket').server;
 var http = require('http');
- 
-var server = http.createServer(function(request, response) {
-	    console.log((new Date()) + ' Received request for ' + request.url);
-	    response.writeHead(404);
-	    response.end();
-});
-server.listen(8080, function() {
-	    console.log((new Date()) + ' Server is listening on port 8080');
-});
- 
-wsServer = new WebSocketServer({
-	    httpServer: server,
-	    autoAcceptConnections: false
-});
- 
-function originIsAllowed(origin) {
-	  return true;
-}
- 
-wsServer.on('request', function(request) {
-	console.log("WebSocket connection accepted.");
+var urlp = require('url');
 
+
+var server = http.createServer(function(request, response) {
+	console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(404);
+	response.end();
+});
+
+server.listen(8080, function() {
+	console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+
+wsServer = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  return true;
+}
+
+var connections = {};
+var connections_circle = [];
+
+wsServer.on('request', function(request) {
 	if (!originIsAllowed(request.origin)) {
 		request.reject();
 		console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
@@ -30,21 +35,49 @@ wsServer.on('request', function(request) {
 	}
 
 	var connection = request.accept(null, request.origin);
+	var url_query =urlp.parse(request.resource,true);
+	 ID and circle_ID
+	var connection_id = url_query.query.id
+	var circle_id = null
+	if(url_query.query.circle)
+		var circle_id = url_query.query.circle
+	
+	connections[url_query.query.id] = connection;
 	console.log((new Date()) + ' Connection accepted.');
+
+
 	connection.on('message', function(message) {
 		if (message.type === 'utf8') {
-			console.log('Received Message: ' + message.utf8Data);
-			connection.sendUTF("EJ");
+			console.log((new Date()) + "ws server received Message:"+message.utf8Data);
+			sendToConnectionId(message.utf8Data,"got");
 		}
 		else if (message.type === 'binary') {
-			console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+			console.log((new Date()) + 'Received Binary Message of ' + message.binaryData.length + ' bytes');
 			connection.sendBytes(message.binaryData);
 		}
 	});
+	
 	connection.on('close', function(reasonCode, description) {
 		console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
 	});
 });
 
 
+function broadcast(data) {
+	Object.keys(connections).forEach(function(key) {
+	var connection = connections[key];
+	if (connection.connected) {
+		connection.send(data);
+	}
+	});
+}
 
+ Send a message to a connection by its connectionID
+ function sendToConnectionId(connectionID, data) {
+	var connection = connections[connectionID];
+	if (connection && connection.connected) {
+		console.log((new Date()) + "Send data to connection ID:"+connectionID);
+		connection.send(data);
+	}
+}
+        
