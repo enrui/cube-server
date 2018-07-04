1720b32e712b6485efe879ae7a269766851ab11f
enrui <enrui.zhang@gmail.com>
2018-07-04 17:04:46 +0800

    Add id config

--- srv/agent/app.js
+++ srv/agent/app.js
@@ -5,9 +5,10 @@ const WebSocket = require('ws');
 var led_pin=2, led_sw=0;
 var led = new onoff_gpio(led_pin, 'out');
 var cube_set = require('/jroot/etc/ENV.json');
+var id_set = require('/jroot/etc/ID.json');
 
 if(cube_set.NOTIFY_SERVER_URL && cube_set.NOTIFY_SERVER_PORT)
-        var ws = new WebSocket('ws://'+cube_set.NOTIFY_SERVER_URL+':'+cube_set.NOTIFY_SERVER_PORT+'?type=agent&id=00000001');
+        var ws = new WebSocket('ws://'+cube_set.NOTIFY_SERVER_URL+':'+cube_set.NOTIFY_SERVER_PORT+'?type=agent&id='+id_set.ID);
 else
         console.log("ws connection setting lost")
 
