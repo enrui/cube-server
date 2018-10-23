8364da2385ea5ccbc0fcdbc3daddb3fe3b039f1b
enrui <enrui.zhang@gmail.com>
2018-10-23 08:14:35 +0000

    Implement login cube system flow

--- srv/agent/app.js
+++ srv/agent/app.js
@@ -1,36 +1,58 @@
-var onoff_gpio = require('onoff').Gpio;
-var led_controller = require( './output/led.js' );
+//var onoff_gpio = require('onoff').Gpio;
+//var led_controller = require( './output/led.js' );
 const WebSocket = require('ws');
+const fs = require("fs");
 
 var led_pin=2, led_sw=0;
-var led = new onoff_gpio(led_pin, 'out');
+//var led = new onoff_gpio(led_pin, 'out');
+var auth = require( './lib/auth.js' );
 var cube_set = require('/jroot/etc/ENV.json');
 var id_set = require('/jroot/etc/ID.json');
 
-if(cube_set.NOTIFY_SERVER_URL && cube_set.NOTIFY_SERVER_PORT)
-        var ws = new WebSocket('ws://'+cube_set.NOTIFY_SERVER_URL+':'+cube_set.NOTIFY_SERVER_PORT+'?type=agent&id='+id_set.ID);
-else
-        console.log("ws connection setting lost")
+var ws=null
 
-var ws_con = null;
+async function cube_login() {
+	let cube_token = await auth.login(id_set.ID,id_set.KEY);
+	
+	if(cube_token == "fail")
+		process.exit(1);
+	
+	fs.writeFile( "../../tmp/.tk", cube_token, function( err ){
+		if(err) 
+			process.exit(1);
+	} );
+	
+	if(cube_set.NOTIFY_SERVER_URL && cube_set.NOTIFY_SERVER_PORT)
+		ws = new WebSocket('ws://'+cube_set.NOTIFY_SERVER_URL+':'+cube_set.NOTIFY_SERVER_PORT+'?type=agent&id='+id_set.ID);
+	else
+		console.log("ws connection setting lost")
+	
+	ws.on('open', function open() {
+		console.log("open connection");
+		//ws.send(1);         
+	});
 
-ws.on('open', function open() {
-	console.log("open connection");
-	//ws.send(1);         
-});
-
-ws.on('message', function incoming(data) {
-	console.log(" client get message:"+data);
-	if("got" == data){
-		if(1==led_sw){
-			led_sw=0
-			led_controller.led_switch(led,1);
-		}else{
-			led_sw=1
-			led_controller.led_switch(led,0);
+	ws.on('message', function incoming(data) {
+		console.log(" client get message:"+data);
+		if("got" == data){
+			if(1==led_sw){
+				led_sw=0
+				//led_controller.led_switch(led,1);
+			}else{
+				led_sw=1
+				//led_controller.led_switch(led,0);
+			}
+			
 		}
-		
-	}
-});
+	});
+
+}
+
+cube_login()
+
+
+
+
+
 
 
--- srv/agent/lib/auth.js
+++ srv/agent/lib/auth.js
@@ -1,9 +1,35 @@
+var request_promise = require('request-promise');
+var cube_set = require('/jroot/etc/ENV.json');
 
-module.exports = function(){
+function get_token_data(did, key){
+	return new Promise((resolve, reject) => {
+		try {
+			var options = {
+				method: 'POST',
+				uri: 'http://'+cube_set.INTERFACE_SERVER_URL+':'+cube_set.INTERFACE_SERVER_PORT+'/device/login',
+				form: {
+					id:did,
+					auth_key:key
+				},
+				json: true // Automatically stringifies the body to JSON
+			};
 
-        this.login = function(tkn){
-                tkn=''
-        }
-
-
+			request_promise(options)
+				.then(function (parsedBody) {
+					resolve(parsedBody.token)
+				})
+				.catch(function (err) {
+					resolve("fail")
+				});			 			
+		} catch ( err ) {
+			reject(err)
+		}
+	})
 }
+
+module.exports = {
+	login : async function(id, key){
+		tkn = await get_token_data(id, key);
+		return tkn
+	}
+};
